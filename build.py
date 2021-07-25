5dfae0fe2a383653958e07594570bacaff30c59a
Brock Fanning <brockfanning@gmail.com>
2021-07-25 17:03:03 -0400

    Fix ref areas

--- build.py
+++ build.py
@@ -1,3 +1,11 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config.yml')
+def alter_data(df):
+    def row_matches_ref_area(row):
+        return row['REF_AREA'] == 'VN'
+
+    df = df.copy()
+    mask = df.apply(row_matches_ref_area, axis=1)
+    return df[mask]
+
+open_sdg_build(config='config.yml', alter_data=alter_data)
